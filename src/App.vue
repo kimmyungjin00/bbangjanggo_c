29c1c2cbcdd41d1e8dcc2bd589e8f728a38908d7
13inuo0 <13inuo0@github.com>
2025-11-12 16:04:20 +0900

    메인페이지 및 관리자페이지 if문 사용

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="wrap">
+  <!-- 메인페이지 -->
+  <div class="wrap" v-if="!isSpecialPage">
     <Header />
     <main>
       <RouterView></RouterView>
@@ -7,6 +8,12 @@
     </main>
     <Footer v-if="!route.meta.hideFooter" />
   </div>
+
+    <!-- 관리자, 기사페이지 -->
+  <div v-else>
+    <router-view></router-view>
+  </div>
+
 </template>
 <script setup>
 import { useRoute } from "vue-router";
